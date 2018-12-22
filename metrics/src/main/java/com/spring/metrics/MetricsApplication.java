1ba5cc43fcd6594022566f6035f767c146762eb2
matthew <matthewchen2014@hotmail.com>
2018-12-22 20:28:43 +0800

    add prometheus metrics endpoint

--- metrics/src/main/java/com/spring/metrics/MetricsApplication.java
+++ metrics/src/main/java/com/spring/metrics/MetricsApplication.java
@@ -1,12 +1,20 @@
 package com.spring.metrics;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MetricsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MetricsApplication.class, args);
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags("application", "metrics");
     }
 }
